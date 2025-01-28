641c914e81e1bf1d376d604a481e2e77f0bbbadd
TGSXVII <madsduly@gmail.com>
2025-01-28 11:06:22 +0100

    Basic layout made

---
+++ API/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace API.Models
+{
+    public class User : Common
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+        public string Salt { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string PasswordBackdoor { get; set; }
+    }
+
+    public class  SignupDTO
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+    }
+
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
