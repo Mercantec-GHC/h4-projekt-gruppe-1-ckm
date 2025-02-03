26c8f9a8df8c32abf70929edc78cc76538a6cfdf
Turbobent <kasp728h@edu.mercantec.dk>
2025-02-03 10:46:22 +0100

    model changes

---
+++ API/Migrations/20250203090241_FixUserQrCodeForeignKeys.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserQrCodeForeignKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserQrCodes_QrCodes_QrCodeId",
+                table: "UserQrCodes");
+
+            migrationBuilder.RenameColumn(
+                name: "QrCodeId",
+                table: "UserQrCodes",
+                newName: "QrId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserQrCodes_QrCodeId",
+                table: "UserQrCodes",
+                newName: "IX_UserQrCodes_QrId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserQrCodes_QrCodes_QrId",
+                table: "UserQrCodes",
+                column: "QrId",
+                principalTable: "QrCodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserQrCodes_QrCodes_QrId",
+                table: "UserQrCodes");
+
+            migrationBuilder.RenameColumn(
+                name: "QrId",
+                table: "UserQrCodes",
+                newName: "QrCodeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserQrCodes_QrId",
+                table: "UserQrCodes",
+                newName: "IX_UserQrCodes_QrCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserQrCodes_QrCodes_QrCodeId",
+                table: "UserQrCodes",
+                column: "QrCodeId",
+                principalTable: "QrCodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ API/Migrations/20250203093434_UserQr-upd.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserQrupd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "UserQrCodes",
+                newName: "User_id");
+
+            migrationBuilder.RenameColumn(
+                name: "QrId",
+                table: "UserQrCodes",
+                newName: "Qr_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User_id",
+                table: "UserQrCodes",
+                newName: "User_idId");
+
+            migrationBuilder.RenameColumn(
+                name: "Qr_id",
+                table: "UserQrCodes",
+                newName: "Qr_idId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserQrCodes_Qr_idId",
+                table: "UserQrCodes",
+                column: "Qr_idId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserQrCodes_User_idId",
+                table: "UserQrCodes",
+                column: "User_idId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserQrCodes_QrCodes_Qr_idId",
+                table: "UserQrCodes",
+                column: "Qr_idId",
+                principalTable: "QrCodes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserQrCodes_Users_User_idId",
+                table: "UserQrCodes",
+                column: "User_idId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
