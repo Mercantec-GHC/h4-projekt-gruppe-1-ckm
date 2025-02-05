349661d337275ce12cf4858017eebd6f9eb87b54
Turbobent <kasp728h@edu.mercantec.dk>
2025-02-05 10:07:20 +0100

    changed comment

--- API/Controllers/QrCodesController.cs
+++ API/Controllers/QrCodesController.cs
@@ -70,7 +70,7 @@ namespace API.Controllers
         public async Task<ActionResult<QrCode>> PostQrCode(QrCodeDTO qrCodePost)
         {
             Regex validateTitle = new(@"^[a-zA-Z0-9]{1,30}$");  // Only letters and numbers (1-30 chars)
-            Regex validateText = new(@"(?:.*?\s)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,10}(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)?)?(?:\s.*)?$"); // Standard link format
+            Regex validateText = new(@"(?:.*?\s)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,10}(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)?)?(?:\s.*)?$"); // Standard link and text format
 
             var errors = new Dictionary<string, string>();
 
