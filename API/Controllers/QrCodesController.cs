0ff2da902a4140bf2d79fb05896ffaf0ab9b9bc2
Turbobent <kasp728h@edu.mercantec.dk>
2025-02-26 12:21:57 +0100

    newer regex

--- API/Controllers/QrCodesController.cs
+++ API/Controllers/QrCodesController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutQrCode(int id, EditQrCode qrCodeEdit)
         {
-            Regex validateTitle = new(@"^[a-zA-Z0-9ÆØÅæøå]{1,30}$");
+            Regex validateTitle = new(@"^[a-zA-Z0-9ÆØÅæøå\s]{1,30}$");
             Regex validateText = new(@"(?:.*?\s)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9ÆØÅæøå@:%._+~#=]{1,256}\.[a-zA-Z0-9ÆØÅæøå()]{1,10}(?:[-a-zA-Z0-9ÆØÅæøå()@:%_+.~#?&//=]*)?)?(?:\s.*)?$");
 
             var errors = new Dictionary<string, string>();
@@ -111,7 +111,7 @@ namespace API.Controllers
             }
 
             // Validation
-            Regex validateTitle = new(@"^[a-zA-Z0-9ÆØÅæøå]{1,30}$");
+            Regex validateTitle = new(@"^[a-zA-Z0-9ÆØÅæøå\s]{1,30}$");
             Regex validateText = new(@"(?:.*?\s)?(https?:\/\/(?:www\.)?[-a-zA-Z0-9ÆØÅæøå@:%._+~#=]{1,256}\.[a-zA-Z0-9ÆØÅæøå()]{1,10}(?:[-a-zA-Z0-9ÆØÅæøå()@:%_+.~#?&//=]*)?)?(?:\s.*)?$");
 
             var errors = new Dictionary<string, string>();
